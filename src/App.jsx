7f201c8cb7eccaf5621a90db0e1b6b56e1523687
Michael Passarge <mpassarge@comcast.net>
2021-04-22 12:34:00 -0400

    Updated Standings Card and section to use antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getStandings } from './api'
 import StandingSection from "./standings/StandingSection";
+import "antd/dist/antd.css";
 
 const App = () => {
 
--- src/standings/StandingsCard.jsx
+++ src/standings/StandingsCard.jsx
@@ -1,80 +1,46 @@
-/** @jsxImportSource @emotion/react */
+import { Col, Table } from 'antd';
 
-import { css } from '@emotion/react'
-import { breakpointUp } from '../mediaQueries';
 
-const containerStyles = (isCombined) => css({
-    border: "1px solid black",
-    ':not(:last-child)': {
-        marginBottom: '2rem',
+const columns = [
+    {
+        title: 'Team',
+        dataIndex: 'name',
+        key: 'name',
+        colspan: 2,
     },
-    'WebkitBoxShadow': '3px 2px 3px 1px rgba(0,0,0,0.56)',
-    'boxShadow': '3px 2px 3px 1px rgba(0,0,0,0.56)',
-    borderRadius: '5px',
-    'h2': {
-        textAlign: 'center',
-        textTransform: 'capitalize',
-        margin: '0.875rem',
-        textDecoration: 'underline',
-        textUnderlineOffset: '2px'
+    {
+        title: 'Points',
+        dataIndex: 'points',
+        key: 'points'
     },
-    'table': {
-        border: '1px solid black',
-        width: '100%',
+    {
+        title: 'Games Played',
+        dataIndex: 'gamesPlayed',
+        key: 'gamesPlayed'
     },
-    'th,td': {
-        padding: '0.1875rem 0rem 0.1875rem 0rem',
-        textAlign: 'left',
-        borderTop: '1px solid black',
-    },
-    'tbody tr:nth-of-type(even)': {
-        backgroundColor: 'rgb(4,30,66, 0.3)',
-        
-    },
-    [`thead tr th:nth-of-type(1), 
-    tbody tr td:nth-of-type(1)`]: {
-        width: '40%',
-    },
-    [breakpointUp('mediumLarge')]: {
-        [`thead tr th:nth-of-type(1), 
-        tbody tr td:nth-of-type(1)`]: {
-            width: '45%',
-        },
-        ':not(:last-child)': {
-            marginBottom: '0rem',
-        },
-        flex: '1 0 40%',
-    },
-    [isCombined && breakpointUp('medium')]: {
-        [`thead tr th:nth-of-type(1), 
-        tbody tr td:nth-of-type(1)`]: {
-            width: '30%',
-        },
-    },
-});
+]
 
-const StandingsCard = ({ divisionName, teams, isCombined }) => {
-    return <div css={containerStyles(isCombined)}>
-        <h2>{divisionName.replace('-', ' ')}</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Team</th>
-                    <th>Points</th>
-                    <th>Games Played</th>
-                </tr>
-            </thead>
-            <tbody>
-                { teams.map(team => 
-                    <tr key={team.name}>
-                        <td>{team.name}</td>
-                        <td>{team.points}</td>
-                        <td>{team.gamesPlayed}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
-    </div>
+const titleStyle = {
+    textTransform: 'capitalize',
+    textAlign: 'center',
+    fontSize: '1.3rem',
 }
 
+const StandingsCard = ({ divisionName, teams, isCombined }) => {
+    const colWidthLarge = isCombined ? 24 : 12;
+    return <>
+        <Col xs={24} lg={colWidthLarge}>
+            <Table 
+                dataSource={teams}
+                columns={columns}
+                bordered
+                size="small"
+                rowKey={row => row.id}
+                title={() => <p style={titleStyle}>{divisionName.replace('-', ' ')}</p>}
+                pagination={false}
+            />
+        </Col>
+        <br/>
+     </>
+}
 export default StandingsCard;
